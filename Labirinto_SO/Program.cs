7c0218defd3d065fc2c28e88d21322da387d5387
Lucas Cotta <158340240+lucascottas@users.noreply.github.com>
2025-10-20 23:30:22 -0300

    Mudança no program

--- Labirinto_SO/Program.cs
+++ Labirinto_SO/Program.cs
@@ -1,20 +1,32 @@
-﻿using System;
+using System;
+using Teste;
 
 class Program
 {
-    private const int LARGURA_LABIRINTO = 51;
-    private const int ALTURA_LABIRINTO = 25;
+    private const int LARGURA_LABIRINTO = 31;
+    private const int ALTURA_LABIRINTO = 15;
 
     static void Main(string[] args)
     {
-  
-
         Labirinto labirinto = new Labirinto(ALTURA_LABIRINTO, LARGURA_LABIRINTO);
 
-        Console.WriteLine("--- Labirinto Aleatório Gerado  ---");
+        Console.WriteLine("--- Labirinto Aleatório Gerado ---");
         labirinto.Imprimir();
-        
-        Console.WriteLine("\nPressione Enter para fechar o programa.");
+
+        Console.WriteLine("\nPressione [R] para soltar o rato...");
+        while (Console.ReadKey(true).Key != ConsoleKey.R) { }
+
+        // rato nasce no canto oposto do queijo
+        Rato rato = new Rato(labirinto, ALTURA_LABIRINTO - 2, LARGURA_LABIRINTO - 2);
+
+        Console.Clear();
+        labirinto.Imprimir();
+        Console.WriteLine("\nRato posicionado! Pressione Enter para começar o movimento...");
+        Console.ReadLine();
+
+        rato.Mover();
+
+        Console.WriteLine("\nPressione Enter para encerrar.");
         Console.ReadLine();
     }
 }
